01a1701227082db3181fcf6541cbf8965b16d069
Ferran <ferran.docencia@gmail.com>
2025-03-26 09:30:45 +0100

    2025-03.26

--- js/script.js
+++ js/script.js
@@ -16,3 +16,29 @@ apellidoHTML.addEventListener('change', () => {
         document.getElementById('errorApellido').textContent = "Hay que introducir un texto de al menos dos caracteres"
     }
 })
+
+// CONTROL DE LAS FECHAS
+const entrada = new Date()
+let salida = new Date(entrada)
+salida.setDate(salida.getDate() + 1)
+// alert(entrada.toISOString())
+
+const entradaString = entrada.toISOString().split('T')[0]
+let salidaString = salida.toISOString().split('T')[0]
+
+const entradaHTML = document.getElementById('entrada')
+entradaHTML.setAttribute('value', entradaString)
+entradaHTML.setAttribute('min', entradaString)
+const salidaHTML = document.getElementById('salida')
+salidaHTML.setAttribute('value', salidaString)
+salidaHTML.setAttribute('min', salidaString)
+
+entradaHTML.addEventListener('change', () => {
+    salida = new Date(entradaHTML.value)
+    salida.setDate(salida.getDate() + 1)
+    salidaString = salida.toISOString().split('T')[0]
+    salidaHTML.setAttribute('value', salidaString)
+    salidaHTML.setAttribute('min', salidaString)
+})
+
+
